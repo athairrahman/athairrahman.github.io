0f1b42d9907b257ebcb8e796bdfc320398fa82f7
AthaJs <athairrahman53@gmail.com>
2022-06-12 00:20:51 +0700

    Menambahkan fitur  input nilai kalkulator

--- script.js
+++ script.js
@@ -1,16 +1,108 @@
+//Defini variabel
+let prevNumber = " "
+let calculationOperator = " "
+let currentNumber ="0"
+
 // mengambil element HTML di code javascript
 const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number)=> {
     //Menambahkan event Clik ke setiap element
     number.addEventListener("click", (event) => {
-        updateSceen(event.target.value);
+        inputNumber(event.target.value);
+        updateScreen(currentNumber)
     })
 })
 
 //untuk memperbaharui layar ketika angka di tekan
 const calculatorScreen = document.querySelector(".calculator-screen");
 
-const updateSceen = (number) => {
+const updateScreen = (number) => {
     calculatorScreen.value = number
 }
+
+//memberikan inputNumber
+const inputNumber = (number) => {
+    //agar 0 hilang ketika di input angka
+    if (currentNumber === "0") {
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+}
+
+//fungsi untuk melakukan operator
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    prevNumber = currentNumber
+    calculationOperator = operator
+    currentNumber = ''
+}
+
+//Mengaktifkan fungsi kalkulasi
+const equalSign = document.querySelector(".equal-sign")
+
+equalSign.addEventListener("click", () => {
+    calculate()
+    updateScreen(currentNumber);
+})
+
+//mengaktifkan operator kalkulasi
+const calculate = () => {
+    let result = ''
+    switch (calculationOperator) {
+        case '+':
+            result = parseFloat(prevNumber) + parseFloat(currentNumber)
+            break
+        case '-':
+            result = parseInt(prevNumber) - parseInt(currentNumber)
+            break
+        case '*':
+            result = parseInt(prevNumber) * parseInt(currentNumber)
+            break
+        case '/':
+            result = parseInt(prevNumber) / parseInt(currentNumber)
+            break
+        default:
+            return
+    }
+    currentNumber = result
+    calculationOperator = ''
+}
+
+//membuat tombol AC berjalan dengan lancar
+const clearBtn = document.querySelector('.all-clear')
+
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
+}
+
+clearBtn.addEventListener('click', () => {
+    clearAll()
+    updateScreen(currentNumber)
+})
+
+
+//membuat function agar bisa menghitung nilai decimal
+const decimal = document.querySelector('.decimal')
+
+inputDecimal = (dot) => {
+    currentNumber += dot
+}
+
+decimal.addEventListener('click', (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
+
+
+
